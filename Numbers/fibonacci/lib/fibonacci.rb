981ef741fb10354d266ee907d438352acf1b3b0c
jbranchaud <jbranchaud@gmail.com>
2014-04-04 09:37:55 -0300

    Adding initial solution. Still needs to be more robust and extra specs are needed.

--- Numbers/fibonacci/lib/fibonacci.rb
+++ Numbers/fibonacci/lib/fibonacci.rb
@@ -1,5 +1,23 @@
 require "fibonacci/version"
 
 module Fibonacci
-  # Your code goes here...
+  
+  # given a positive integer, n, this method will generate a string that is
+  # a fibonacci sequence based at 0 up to the nth value. It will be comma
+  # separated. For example, given 3, the returned value will be "0,1,1,2".
+  def self.compute_to_nth(n)
+    sequence_array = Array.new
+    (0..n).each do |x|
+      sequence_array.push(self.compute_nth(x))
+    end
+    sequence_array.join(',')
+  end
+
+  def self.compute_nth(n)
+    return "error" if n < 0
+    return 0 if n == 0
+    return 1 if n == 1
+    return self.compute_nth(n-1) + self.compute_nth(n-2)
+  end
+
 end
