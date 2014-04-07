8babd16528aad4b9eda0f281b509e08be1259a45
jbranchaud <jbranchaud@gmail.com>
2014-04-06 22:45:47 -0300

    Updating the initial spec.

--- Numbers/prime_factors/spec/prime_factor_spec.rb
+++ Numbers/prime_factors/spec/prime_factor_spec.rb
@@ -8,12 +8,14 @@ describe PrimeFactors do
 
   describe 'is_prime?' do
     
-    it "should recognize primes" do
+    it "should return true given a prime number" do
       PrimeFactors.is_prime?(2).should eq(true)
       PrimeFactors.is_prime?(3).should eq(true)
       PrimeFactors.is_prime?(5).should eq(true)
       PrimeFactors.is_prime?(11).should eq(true)
       PrimeFactors.is_prime?(17).should eq(true)
+      PrimeFactors.is_prime?(131).should eq(true)
+      PrimeFactors.is_prime?(997).should eq(true)
     end
 
   end
