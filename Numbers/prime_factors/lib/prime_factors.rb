37993d7fdcedbb77aa9ea024fd064c162e243811
jbranchaud <jbranchaud@gmail.com>
2014-04-06 23:15:00 -0300

    Adding code for the is_prime? function

--- Numbers/prime_factors/lib/prime_factors.rb
+++ Numbers/prime_factors/lib/prime_factors.rb
@@ -3,8 +3,15 @@ require "prime_factors/version"
 module PrimeFactors
 
   # determine if a given integer, n, is a prime number
+  # if n <= 1, then return false
+  # if n % m == 0 for any integer m, where 2 <= m < n, return false
+  # otherwise return true
   def self.is_prime?(n)
-    true
+    return false if n <= 1
+    (2...n).each do |m|
+      return false if n % m == 0
+    end
+    return true
   end
 
 end
