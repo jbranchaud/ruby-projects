609a4579e22faf3076c329832d626b41fc71dd6b
jbranchaud <jbranchaud@gmail.com>
2014-04-06 22:08:32 -0300

    Adding some initial specs for prime checking.

--- Numbers/prime_factors/lib/prime_factors.rb
+++ Numbers/prime_factors/lib/prime_factors.rb
@@ -1,5 +1,10 @@
 require "prime_factors/version"
 
 module PrimeFactors
-  # Your code goes here...
+
+  # determine if a given integer, n, is a prime number
+  def self.is_prime?(n)
+    true
+  end
+
 end
--- Numbers/prime_factors/spec/prime_factor_spec.rb
+++ Numbers/prime_factors/spec/prime_factor_spec.rb
@@ -6,4 +6,16 @@ describe PrimeFactors do
     true
   end
 
+  describe 'is_prime?' do
+    
+    it "should recognize primes" do
+      PrimeFactors.is_prime?(2).should eq(true)
+      PrimeFactors.is_prime?(3).should eq(true)
+      PrimeFactors.is_prime?(5).should eq(true)
+      PrimeFactors.is_prime?(11).should eq(true)
+      PrimeFactors.is_prime?(17).should eq(true)
+    end
+
+  end
+
 end
