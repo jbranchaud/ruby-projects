da3bdc1a597fd6987e909f8620893d46da3ce374
jbranchaud <jbranchaud@gmail.com>
2014-04-06 23:43:46 -0300

    Implemented the least_divisor method.

--- Numbers/prime_factors/lib/prime_factors.rb
+++ Numbers/prime_factors/lib/prime_factors.rb
@@ -14,4 +14,15 @@ module PrimeFactors
     return true
   end
 
+  # determine the least divisor (excluding 1) of a given integer, n
+  # if n <= 1, then return "error"
+  # otherwise if n % m = 0 for an integer m, where 2 <= m <= n, return m
+  # this requires that we start m at 2 and increment
+  def self.least_divisor(n)
+    return "error" if n <= 1
+    (2..n).each do |m|
+      return m if n % m == 0
+    end
+  end
+
 end
