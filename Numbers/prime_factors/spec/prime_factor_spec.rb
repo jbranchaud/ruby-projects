c4112c55fe596156e0f837609b314f1e85008bb1
jbranchaud <jbranchaud@gmail.com>
2014-04-06 22:50:02 -0300

    DRYing out one of the specs.

--- Numbers/prime_factors/spec/prime_factor_spec.rb
+++ Numbers/prime_factors/spec/prime_factor_spec.rb
@@ -9,13 +9,10 @@ describe PrimeFactors do
   describe 'is_prime?' do
     
     it "should return true given a prime number" do
-      PrimeFactors.is_prime?(2).should eq(true)
-      PrimeFactors.is_prime?(3).should eq(true)
-      PrimeFactors.is_prime?(5).should eq(true)
-      PrimeFactors.is_prime?(11).should eq(true)
-      PrimeFactors.is_prime?(17).should eq(true)
-      PrimeFactors.is_prime?(131).should eq(true)
-      PrimeFactors.is_prime?(997).should eq(true)
+      primes = [2,3,5,11,17,131,997]
+      primes.each do |prime|
+        PrimeFactors.is_prime?(prime).should eq(true)
+      end
     end
 
   end
