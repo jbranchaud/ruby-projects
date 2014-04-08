41604e06763c3d655ac8fbb9b2fad20426f19c24
jbranchaud <jbranchaud@gmail.com>
2014-04-08 16:12:48 -0300

    Expanding the command line interface to give the user more options of ways to count vowels.

--- Text/count_vowels/lib/count_vowels.rb
+++ Text/count_vowels/lib/count_vowels.rb
@@ -24,10 +24,35 @@ module CountVowels
 
   # prompt the user for a string to do vowel count on
   def self.prompt_user
+    puts "## Count Vowels ##"
+    puts "1. count all the vowels in a given string."
+    puts "2. count occurrences of each vowel in a given string."
+    print "Choose an option [1/2] "
+    option = gets.chomp.to_i
+    
+    if option == 1
+      self.prompt_count_vowels
+    elsif option == 2
+      self.prompt_count_each_vowel
+    else
+      puts "Invalid option selected, exiting."
+    end
+  end
+
+  # prompt for a string to run through count_vowels
+  def self.prompt_count_vowels
     print "Enter a string, we will count its vowels: "
     string = gets.chomp
 
     puts self.count_vowels(string)
+  end
+
+  # prompt for a string to run through count_each_vowel
+  def self.prompt_count_each_vowel
+    print "Enter a string, we will produce a count of each vowel's occurrence: "
+    string = gets.chomp
+
+    puts self.count_each_vowel(string)
   end
 
 end
