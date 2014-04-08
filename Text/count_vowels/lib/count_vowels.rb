7556bcfe287728bcb729d78aa2f8cc04b9978c71
jbranchaud <jbranchaud@gmail.com>
2014-04-08 16:00:28 -0300

    Implemented a method that returns the respective counts of the vowels.

--- Text/count_vowels/lib/count_vowels.rb
+++ Text/count_vowels/lib/count_vowels.rb
@@ -8,6 +8,20 @@ module CountVowels
     string.scan(/[aeiouAEIOU]/).size
   end
 
+  # given a string, count the occurrences of each vowel (lower and upper
+  # case) in the string and return the counts in a hash where the keys are
+  # each of the vowels that appear and the values are the respective
+  # counts.
+  def self.count_each_vowel(string)
+    vowel_hash = {}
+    string.split("").each do |char|
+      if char =~ /[aeiouAEIOU]/
+        vowel_hash[char] = 1 + (vowel_hash[char] || 0)
+      end
+    end
+    return vowel_hash
+  end
+
   # prompt the user for a string to do vowel count on
   def self.prompt_user
     print "Enter a string, we will count its vowels: "
