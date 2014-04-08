c9018fd21d6103c2a23dd93e49eb7dcb67c3fd1b
jbranchaud <jbranchaud@gmail.com>
2014-04-08 11:16:01 -0300

    Adding a prompt_user method that can be called directly or as default rake task.

--- Text/count_vowels/lib/count_vowels.rb
+++ Text/count_vowels/lib/count_vowels.rb
@@ -8,4 +8,17 @@ module CountVowels
     string.scan(/[aeiouAEIOU]/).size
   end
 
+  # prompt the user for a string to do vowel count on
+  def self.prompt_user
+    print "Enter a string, we will count its vowels: "
+    string = gets.chomp
+
+    puts self.count_vowels(string)
+  end
+
+end
+
+# if file is run directly, prompt the user
+if __FILE__==$0
+  CountVowels.prompt_user
 end
