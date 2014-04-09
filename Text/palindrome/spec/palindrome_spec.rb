c9a75563a48ffa1a1043fe79e0de5425fc61c0cd
jbranchaud <jbranchaud@gmail.com>
2014-04-09 09:01:32 -0300

    Adding specs for testing palindrome? method.

--- Text/palindrome/spec/palindrome_spec.rb
+++ Text/palindrome/spec/palindrome_spec.rb
@@ -1,11 +1,23 @@
 require 'spec_helper'
 
 describe Palindrome do
-  it 'should have a version number' do
-    Palindrome::VERSION.should_not be_nil
+
+  describe "palindrome?" do
+    
+    it "should return true when given a string that is the same forwards and backwards" do
+      strings = ["","R","racecar","deified","amanaplanacanalpanama","AMANAPLANACANALPANAMA","1234567654321"]
+      strings.each do |string|
+        Palindrome.palindrome?(string).should == true
+      end
+    end
+
+    it "should return false when given a string that isn't the same forwards and backwards" do
+      strings = ["#bob","palindrome","google","AManAPlanACanalPanama","A Man A Plan A Canal Panama","YOLO"]
+      strings.each do |string|
+        Palindrome.palindrome?(string).should == false
+      end
+    end
+
   end
 
-  it 'should do something useful' do
-    false.should eq(true)
-  end
 end
