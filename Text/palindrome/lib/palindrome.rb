f2ca7c3b208fea108025ecd6bbc05f27ac291515
jbranchaud <jbranchaud@gmail.com>
2014-04-09 09:12:13 -0300

    Implemented the palindrome? method.

--- Text/palindrome/lib/palindrome.rb
+++ Text/palindrome/lib/palindrome.rb
@@ -1,5 +1,19 @@
-require "palindrome/version"
+# Palindrome
 
 module Palindrome
-  # Your code goes here...
+
+  # given a string, determine if it is a palindrome by verifying that it
+  # reads the same forwards and backwards.
+  def self.palindrome?(string)
+    #string == string.reverse
+    m = 0
+    n = string.length - 1
+    while m < n do
+      return false if string[m] != string[n]
+      m += 1
+      n -= 1
+    end
+    return true
+  end
+
 end
