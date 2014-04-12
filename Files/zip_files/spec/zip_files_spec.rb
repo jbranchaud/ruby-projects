86926911f8ea26fed7611593ccb70c0a402b7940
jbranchaud <jbranchaud@gmail.com>
2014-04-11 23:38:45 -0300

    Removing the default tests from the spec file.

--- Files/zip_files/spec/zip_files_spec.rb
+++ Files/zip_files/spec/zip_files_spec.rb
@@ -1,11 +1,7 @@
 require 'spec_helper'
 
 describe ZipFiles do
-  it 'has a version number' do
-    expect(ZipFiles::VERSION).not_to be nil
-  end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
+  
+
 end
