4d0d89e9552eea1a60034851cc81b214e9cb8e00
jbranchaud <jbranchaud@gmail.com>
2014-04-12 10:32:28 -0300

    A simple zipping function is working. Now the functionality needs to be expanded.

--- Files/zip_files/lib/zip_files.rb
+++ Files/zip_files/lib/zip_files.rb
@@ -1,5 +1,24 @@
-require "zip_files/version"
+require 'zip'
 
 module ZipFiles
-  # Your code goes here...
+
+  # zip the files in the current directory using the rubyzip gem
+  def self.zip_current_directory_files
+    folder = "./"
+    input_filenames = ['README.md']
+    zipfile_name = "./ZipFiles.zip"
+
+    Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
+      input_filenames.each do |filename|
+        zipfile.add(filename, folder + filename)
+      end
+    end
+  end
+
+end
+
+if __FILE__==$0
+  puts "About to zip the current directory files"
+  ZipFiles.zip_current_directory_files
+  puts "Done zipping the current directory files"
 end
