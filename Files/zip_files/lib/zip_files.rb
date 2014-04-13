b2a34ecd3b58d613fa012f1ea0cf2d959acc64bc
jbranchaud <jbranchaud@gmail.com>
2014-04-13 15:24:40 -0300

    Option parsing has been setup using OptParse.

--- Files/zip_files/lib/zip_files.rb
+++ Files/zip_files/lib/zip_files.rb
@@ -29,12 +29,38 @@ module ZipFiles
     Dir["#{directory_pathname.realpath}/*"].find_all { |directory| File.directory?(directory) }
   end
 
+  # list everything in a directory including hidden files/directories
+  def self.list_everything_in_directory(directory=".")
+    directory_pathname = Pathname.new(directory)
+    Dir.glob("#{directory_pathname}/*", File::FNM_DOTMATCH).find_all{ |file| file } - %w[./. ./..]
+  end
+
 end
 
 if __FILE__==$0
-  p "Files: #{ZipFiles.list_files_in_directory("./")}"
-  p "Directories: #{ZipFiles.list_directories_in_directory("./")}"
-  puts "About to zip the current directory files"
-  ZipFiles.zip_current_directory_files
-  puts "Done zipping the current directory files"
+
+  # parse argument options
+  options = {}
+  OptionParser.new do |opts|
+    opts.banner = "Usage: zip_files.rb [options]"
+
+    opts.on("-o [ZIP_PATH]", "--output [ZIP_PATH]", "Path of the output zip file, this is optional, so if not specified, a zip file with the name 'zipped.zip' will be saved to the current directory.") do |o|
+      options[:output] = o
+    end
+
+  end.parse!
+
+  if ARGV.length < 1
+    abort("A list of files and/or directories must be specified for zipping. Exiting.")
+  end
+
+  options[:file_list] = ARGV
+  p options
+
+  #p "Files: #{ZipFiles.list_files_in_directory("./")}"
+  #p "Directories: #{ZipFiles.list_directories_in_directory("./")}"
+  #p ZipFiles.list_everything_in_directory
+  #puts "About to zip the current directory files"
+  #ZipFiles.zip_current_directory_files
+  #puts "Done zipping the current directory files"
 end
