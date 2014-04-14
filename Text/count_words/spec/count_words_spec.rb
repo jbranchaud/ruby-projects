f2fbe19fdcccb9fb144f3ebffba1497ba9a6b80f
jbranchaud <jbranchaud@gmail.com>
2014-04-14 13:55:30 -0300

    Adding a spec for the simple word count.

--- Text/count_words/spec/count_words_spec.rb
+++ Text/count_words/spec/count_words_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 describe CountWords do
-  it 'has a version number' do
-    expect(CountWords::VERSION).not_to be nil
+
+  describe 'simple_word_count' do
+    test_string_hash = {
+      "ruby" => 1,
+      "hello, world!" => 2,
+      "hyphenated-word" => 1,
+      "It is a joy to write Ruby code." => 8,
+      "I'd say this string isn't what it seems." => 8,
+      "The K&R book is a good C resource." => 8,
+      "What... what are you talking about?" => 6,
+      "This string is going to be a lot longer than some of the strings we have seen so far. It is even going to have more than one sentence. It might even have a third sentence with some [...] content that has been excluded." => 44
+    }
+    test_string_hash.each do |string,count|
+      CountWords.simple_word_count(string).should eq(count)
+    end
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
