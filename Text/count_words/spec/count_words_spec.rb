d2f2df1aacf6e805b8cc38e29e0fcbf62e0135be
jbranchaud <jbranchaud@gmail.com>
2014-04-14 14:11:45 -0300

    Fixing the spec, I originally wrote it incorrectly.

--- Text/count_words/spec/count_words_spec.rb
+++ Text/count_words/spec/count_words_spec.rb
@@ -3,19 +3,23 @@ require 'spec_helper'
 describe CountWords do
 
   describe 'simple_word_count' do
-    test_string_hash = {
-      "ruby" => 1,
-      "hello, world!" => 2,
-      "hyphenated-word" => 1,
-      "It is a joy to write Ruby code." => 8,
-      "I'd say this string isn't what it seems." => 8,
-      "The K&R book is a good C resource." => 8,
-      "What... what are you talking about?" => 6,
-      "This string is going to be a lot longer than some of the strings we have seen so far. It is even going to have more than one sentence. It might even have a third sentence with some [...] content that has been excluded." => 44
-    }
-    test_string_hash.each do |string,count|
-      CountWords.simple_word_count(string).should eq(count)
+
+    it "should return a simple word count for a given string" do
+      test_string_hash = {
+        "ruby" => 1,
+        "hello, world!" => 2,
+        "hyphenated-word" => 1,
+        "It is a joy to write Ruby code." => 8,
+        "I'd say this string isn't what it seems." => 8,
+        "The K&R book is a good C resource." => 8,
+        "What... what are you talking about?" => 6,
+        "This string is going to be a lot longer than some of the strings we have seen so far. It is even going to have more than one sentence. It might even have a third sentence with some [...] content that has been excluded." => 44
+      }
+      test_string_hash.each do |string,count|
+        CountWords.simple_word_count(string).should eq(count)
+      end
     end
+
   end
 
 end
