b4b6eb8a8f526d048998816977c36799f8842cd6
jbranchaud <jbranchaud@gmail.com>
2014-04-14 15:01:38 -0300

    Adding docs for the simple_word_count method.

--- Text/count_words/lib/count_words.rb
+++ Text/count_words/lib/count_words.rb
@@ -2,6 +2,8 @@ require "count_words/version"
 
 module CountWords
 
+  # given a string, this method computes a simple word count by splitting on
+  # spaces
   def self.simple_word_count(string)
     string.split.length
   end
