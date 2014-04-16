1ba088784fe00d659d5660821e461b2aa5cfbe2b
jbranchaud <jbranchaud@gmail.com>
2014-04-15 23:06:44 -0300

    Adding the simple_word_summary method, it passes the spec.

--- Text/count_words/lib/count_words.rb
+++ Text/count_words/lib/count_words.rb
@@ -8,6 +8,17 @@ module CountWords
     string.split.length
   end
 
+  # given a string, this method computes a simple word summary that contains
+  # each unique word mapped to the number of occurrences of that word in the
+  # string. Returns a hash containing this association.
+  def self.simple_word_summary(string)
+    summary_hash = {}
+    string.split.each do |word|
+      summary_hash[word] = (summary_hash[word] || 0) + 1
+    end
+    summary_hash
+  end
+
   # given a string, this method computes a more advanced word count by
   # considering punctuation and non-word entities.
   def self.word_count(string)
