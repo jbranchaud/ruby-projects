beab9339063af93c00758b4c43db39750e8cb580
jbranchaud <jbranchaud@gmail.com>
2014-04-21 20:08:01 -0300

    Add an implementation of the offset_letter method.

--- Security/caesar_cipher/lib/caesar_cipher.rb
+++ Security/caesar_cipher/lib/caesar_cipher.rb
@@ -1,5 +1,21 @@
-require "caesar_cipher/version"
+# Caesar Cipher
 
 module CaesarCipher
-  # Your code goes here...
+
+  def self.offset_letter(letter,offset)
+    # scrub the offset, mod it by 26 to make sure we are working with
+    # something between 0 and 25. Modding works because with caesar cipher,
+    # it would just wrap anyway and end up with the same result.
+    normal_offset = offset % 26
+
+    # ensure that we are working with an uppercase letter
+    upper_letter = letter.upcase
+
+    # A is 65, Z is 90
+    # Subtract 65 (our base) from the #ord of letter, add the offset, mod by
+    # 26 to make sure we are operating between 0 and 25, then add 65 back
+    # into the equation, then convert to a UTF-8 character.
+    ((( upper_letter.ord - 65 + normal_offset ) % 26 ) + 65 ).chr("UTF-8")
+  end
+
 end
