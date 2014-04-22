cd019c4067d4565098d57f41c387767a6c7937a2
jbranchaud <jbranchaud@gmail.com>
2014-04-22 01:02:46 -0300

    Add the implementation for the decode_letter method.

--- Security/caesar_cipher/lib/caesar_cipher.rb
+++ Security/caesar_cipher/lib/caesar_cipher.rb
@@ -29,4 +29,20 @@ module CaesarCipher
     end.join
   end
 
+  # decode a given letter using the given offset value, return the resulting
+  # decoded letter.
+  def self.decode_letter(letter,offset)
+    # if it isn't an alphabet character, return it as is
+    return letter if letter =~ /[^a-zA-Z]/
+
+    # ensure that we are working with an uppercase letter
+    upper_letter = letter.upcase
+
+    # A is 65, Z is 90
+    # Subtract 65 (our base) from the #ord of letter, subtract the offset, mod
+    # by 26 to make sure we are operating between 0 and 25, then add 65 back
+    # into the equation, then convert to a UTF-8 character
+    ((( upper_letter.ord - 65 - offset ) % 26 ) + 65 ).chr("UTF-8")
+  end
+
 end
