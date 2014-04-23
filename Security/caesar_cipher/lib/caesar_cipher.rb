9beff2303d6ee2e12771f640748f22bd05d96b52
jbranchaud <jbranchaud@gmail.com>
2014-04-22 22:24:54 -0300

    Add the decode command line option and the functionality to decode an encoded file.

--- Security/caesar_cipher/lib/caesar_cipher.rb
+++ Security/caesar_cipher/lib/caesar_cipher.rb
@@ -73,6 +73,23 @@ module CaesarCipher
     end.join
   end
 
+  # decode the given file with the given offset value, the decoded file content
+  # should be written to the given output file. If no output file is given,
+  # then write it to a new file with '.dec' appended to the end of the end of
+  # the input file's name (e.g. 'input.txt' -> 'input.txt.dec').
+  def self.decode_file(offset,input_filename,output_filename="")
+    # if no output file is given, then create one
+    if output_filename = ""
+      output_filename = input_filename + '.dec'
+    end
+
+    File.open(output_filename, 'w') do |output_file|
+      File.readlines(input_filename).each do |input_line|
+        output_file.write( self.decode( input_line, offset ) )
+      end
+    end
+  end
+
 end
 
 # if file is run directly, grab command line argument and invoke either the
@@ -86,7 +103,16 @@ if __FILE__==$0
   OptionParser.new do |opts|
     opts.banner = "Usage: caesar_cipher.rb [-e|-d] [-o output] num filename"
 
-    # ignore optional arguments for the moment
+    # check for the encode option (defaults on though)
+    opts.on("-e", "--encode", "Encode the given file with the given offset value.") do |e|
+      options[:encode] = true
+    end
+
+    # check for the decode option
+    opts.on("-d", "--decode", "Decode the given file with the given offset value.") do |d|
+      options[:encode] = false
+    end
+
   end.parse!
 
   if ARGV.length < 2
@@ -97,6 +123,10 @@ if __FILE__==$0
   options[:input] = Pathname.new(ARGV[1]).realdirpath.to_s
   p options
 
-  CaesarCipher.encode_file(options[:offset],options[:input],options[:output])
+  if options[:encode]
+    CaesarCipher.encode_file(options[:offset],options[:input],options[:output])
+  else
+    CaesarCipher.decode_file(options[:offset],options[:input],options[:output])
+  end
 
 end
