fb81b47d9b44a71fd7018ee390bc9c14b5c5d404
jbranchaud <jbranchaud@gmail.com>
2014-04-26 01:40:47 -0300

    Move node to the graph class, remove node.rb from the picture.

--- lib/graph/graph.rb
+++ lib/graph/graph.rb
@@ -1,10 +1,30 @@
-require './node.rb'
+
+# graph.rb
+#
+# a class that represents a graph
 
 class Graph
 
+  attr_accessor :name, :nodes
   def initialize(name)
     @name = name
     @nodes = [] # empty list of nodes
+  end
+
+end
+
+
+# node
+#
+# a class that represents a node in a graph
+
+class Node
+
+  attr_accessor :value, :incoming, :outgoing
+  def initialize(value)
+    @value = value
+    @incoming = []
+    @outgoing = []
   end
 
 end
