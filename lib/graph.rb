d14cc6ad38e03b6822cb5ba98e74151d6aab716e
jbranchaud <jbranchaud@gmail.com>
2014-04-27 19:39:05 -0300

    Wrap a module around the graph and node class.

--- lib/graph.rb
+++ lib/graph.rb
@@ -1,7 +1,7 @@
 require 'graph/graph.rb'
 
-g1 = Graph.new('g1')
+g1 = Graph::Graph.new('g1')
 p g1
 
-g1.nodes.push(Node.new(5))
+g1.nodes.push(Graph::Node.new(5))
 p g1
--- lib/graph/graph.rb
+++ lib/graph/graph.rb
@@ -1,30 +1,32 @@
+# graph module
+module Graph
 
-# graph.rb
-#
-# a class that represents a graph
-
-class Graph
-
-  attr_accessor :name, :nodes
-  def initialize(name)
-    @name = name
-    @nodes = [] # empty list of nodes
+  # graph.rb
+  #
+  # a class that represents a graph
+  class Graph
+  
+    attr_accessor :name, :nodes
+    def initialize(name)
+      @name = name
+      @nodes = [] # empty list of nodes
+    end
+  
   end
-
-end
-
-
-# node
-#
-# a class that represents a node in a graph
-
-class Node
-
-  attr_accessor :value, :incoming, :outgoing
-  def initialize(value)
-    @value = value
-    @incoming = []
-    @outgoing = []
+  
+  
+  # node
+  #
+  # a class that represents a node in a graph
+  class Node
+  
+    attr_accessor :value, :incoming, :outgoing
+    def initialize(value)
+      @value = value
+      @incoming = []
+      @outgoing = []
+    end
+  
   end
 
 end
