67f56a8a3a6053f9acb33053b29b510950c76dd6
jbranchaud <jbranchaud@gmail.com>
2014-04-27 23:51:07 -0300

    Add a spec for the graph instantiation (new method).

--- spec/graph_spec.rb
+++ spec/graph_spec.rb
@@ -2,4 +2,14 @@ require 'spec_helper'
 
 describe Graph do
 
+  describe 'new' do
+
+    it "should create a new graph with a name and no nodes" do
+      graph1 = Graph::Graph.new('graph1')
+      graph1.name.should eq('graph1')
+      graph1.nodes.empty?.should eq(true)
+    end
+
+  end
+
 end
