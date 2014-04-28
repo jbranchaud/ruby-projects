b752c6b662b1d20783608c070c81e7e6a0e3daa1
jbranchaud <jbranchaud@gmail.com>
2014-04-28 14:43:46 -0300

    Add the graph class layer nested within the graph module for the spec. It will now be easy to differentiate between specs for the node and specs for the graph class.

--- spec/graph_spec.rb
+++ spec/graph_spec.rb
@@ -2,12 +2,16 @@ require 'spec_helper'
 
 describe Graph do
 
-  describe 'new' do
+  describe 'Graph' do
 
-    it "should create a new graph with a name and no nodes" do
-      graph1 = Graph::Graph.new('graph1')
-      graph1.name.should eq('graph1')
-      graph1.nodes.empty?.should eq(true)
+    describe 'new' do
+
+      it "should create a new graph with a name and no nodes" do
+        graph1 = Graph::Graph.new('graph1')
+        graph1.name.should eq('graph1')
+        graph1.nodes.empty?.should eq(true)
+      end
+
     end
 
   end
