5d25463e6dee559e1730275807624f2b13eab31f
jbranchaud <jbranchaud@gmail.com>
2014-05-23 09:01:45 -0300

    Add some extra comments to explaining everything happening in the code.

--- Text/reverse_string/lib/reverse_string.rb
+++ Text/reverse_string/lib/reverse_string.rb
@@ -2,10 +2,12 @@
 
 module ReverseString
 
+  # basic string reversal, nothing fancy
   def self.reverse_string(string)
     string.reverse
   end
 
+  # prompt the user for a string to reverse
   def self.prompt_user
     print 'Enter a string to be reversed: '
     input_string = gets.chomp
@@ -14,6 +16,7 @@ module ReverseString
 
 end
 
+# this bit is run if the file is invoked directly
 if __FILE__==$0
   ReverseString.prompt_user
 end
