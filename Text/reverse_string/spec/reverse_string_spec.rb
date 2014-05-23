b791135d70a5641c4111babd462b55adfdfdaa1e
jbranchaud <jbranchaud@gmail.com>
2014-05-23 08:50:05 -0300

    Add a spec for the reverse_string method.

--- Text/reverse_string/spec/reverse_string_spec.rb
+++ Text/reverse_string/spec/reverse_string_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 describe ReverseString do
-  it 'has a version number' do
-    expect(ReverseString::VERSION).not_to be nil
-  end
+  
+  describe 'reverse_string' do
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+    it 'should reverse the given string' do
+      string_reversing_hash = {
+        "hello" => "olleh",
+        "abc" => "cba",
+        "" => "",
+        "Z" => "Z",
+        "and even a longer string" => "gnirts regnol a neve dna"
+      }
+      string_reversing_hash.each do |original_string,reverse_string|
+        ReverseString.reverse_string(original_string).should eq(reverse_string)
+      end
+    end
+
   end
 end
